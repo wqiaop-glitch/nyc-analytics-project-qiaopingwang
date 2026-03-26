827ea3b91b9817a254f435f27a6e97faabf5516d
Qiaoping Wang <qiaoping.wang@baruchmail.cuny.edu>
2026-03-26 20:15:14 +0000

    transform data

--- models/assignment_work/test_311_sample.sql/test_311_sample.sql
+++ models/assignment_work/test_311_sample.sql/test_311_sample.sql
@@ -1,8 +1,4 @@
 -- Quick test to verify source connection works
- SELECT
-     unique_key,
-     created_date,
-     complaint_type,
-     borough
- FROM {{ source('raw', 'source_dot_service_requests_history') }}
- LIMIT 10
+select unique_key, created_date, complaint_type, borough
+from {{ source("raw", "source_dot_service_requests_history") }}
+limit 10
